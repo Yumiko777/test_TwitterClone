1ded72948c9d438a8356881f8c4138f7165014be
Yumiko <wakinghira73@gmail.com>
2021-05-07 16:14:17 +0900

    エラー修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,58 +1,57 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
-　　before_action :set_post, only: [:show, :edit, :update, :destroy]
+  def index
+    @posts = Post.all
+  end
 
-    def index
-      @posts = Post.all
-    end
+  def new
+    @post = Post.new
+  end
 
-    def new
-      @post = Post.new
-    end
-
-    def create
-      @post = Post.create(post_params)
-      if params[:back]
+  def create
+    @post = Post.create(post_params)
+    if params[:back]
+      render :new
+    else
+      if @post.save
+        redirect_to new_post_path, notice: "つぶやきを投稿しました！"
+      else
         render :new
-      else
-        if @post.save
-          redirect_to new_post_path, notice: "つぶやきを投稿しました！"
-        else
-          render :new
-        end
       end
     end
+  end
 
-    def show
-    end
+  def show
+  end
 
-    def edit
-    end
+  def edit
+  end
 
-    def update
-      if @post.update(post_params)
-        redirect_to post_path, notice: "つぶやきを編集しました！"
-      else
-        render :edit
-      end
+  def update
+    if @post.update(post_params)
+      redirect_to post_path, notice: "つぶやきを編集しました！"
+    else
+      render :edit
     end
+  end
 
-    def destroy
-      @post.destroy
-      redirect_to posts_path, notice:"つぶやきを削除しました！"
-    end
+  def destroy
+    @post.destroy
+    redirect_to posts_path, notice:"つぶやきを削除しました！"
+  end
 
-    def confirm
-      @post = Post.new(post_params)
-      render :new if @post.invalid?
-    end
+  def confirm
+    @post = Post.new(post_params)
+    render :new if @post.invalid?
+  end
 
-    private
-    def post_params
-      params.require(:post).permit(:content)
-    end
+  private
+  def post_params
+    params.require(:post).permit(:content)
+  end
 
-    def set_post
-      @post = Post.find(params[:id])
-    end
+  def set_post
+    @post = Post.find(params[:id])
+  end
 end
